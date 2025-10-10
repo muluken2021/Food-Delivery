0634a40f9d6667dccbd41b010888dec7a766cb68
Muluken <mulukenkassaw2021@gmail.com>
2025-10-10 17:10:36 +0300

    Delete Backend directory

--- Backend/config/Db.js
+++
@@ -1,7 +0,0 @@
-import mongoose  from 'mongoose'
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://muluken:mongoo123@cluster0.on9rqot.mongodb.net/Food-Del').then(()=>{
-        console.log('DB connected')
-    });
-}
